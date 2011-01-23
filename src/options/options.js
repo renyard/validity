02d47c0c2796f9908aee7d93928ac2add5b1241d
= <ian@renyard.net>
2011-01-23 16:38:53 +0000

    Refactored options code and completed options UI changes.

--- src/options/options.js
+++ src/options/options.js
@@ -1,62 +1,137 @@
 (function() {
-	var enableHosts = document.getElementById('enableHosts'),
-		validateHostsElm = document.getElementById('validateHosts'),
-		validateHosts,
-		hostOpt,
-		validator = document.getElementById('validator'),
-		collapsed = document.getElementById('collapse');
+	var $ = function(id) {return document.getElementById(id)},
+		enableHostsElm,
+		validateHostsElm,
+		validateHost;
 
-	//	Load options
-	window.addEventListener('load', function() {
-		var validateHosts = [];
+	function loadOptions() {
+		var validateHosts,
+			hostOpt,
+			validator = $('validator'),
+			collapsed = $('collapse');
+	
+		//	Load options
+		window.addEventListener('load', function() {
+			var enableHosts = [],
+				validateHosts = [];
 
-		if (localStorage['enableHosts']) {
-			enableHosts.value = localStorage['enableHosts'];
-		}
-		if (localStorage['validateHosts'] !== undefined) {
-			validateHosts = localStorage['validateHosts'].split(' ');
-			for (var i = 0; i < validateHosts.length; i++) {
-				hostOpt = document.createElement('option');
-				hostOpt.textContent = validateHosts[i];
-				validateHostsElm.appendChild(hostOpt);
+			//	TODO: abstract select box population into function
+			if (localStorage['enableHosts']) {
+				enableHosts = localStorage['enableHosts'].split(' ');
+				for (var i = 0; i < enableHosts.length; i++) {
+					hostOpt = document.createElement('option');
+					hostOpt.textContent = enableHosts[i];
+					enableHostsElm.appendChild(hostOpt);
+				}
+			}
+
+			if (localStorage['validateHosts'] !== undefined) {
+				validateHosts = localStorage['validateHosts'].split(' ');
+				//	Loop through hosts to populate select box
+				for (var j = 0; j < validateHosts.length; j++) {
+					hostOpt = document.createElement('option');
+					hostOpt.textContent = validateHosts[j];
+					validateHostsElm.appendChild(hostOpt);
+				}
+			}
+
+			if (localStorage['validator'] !== undefined) {
+				validator.value = localStorage['validator'];
+			}
+
+			if (localStorage['collapseResults'] !== undefined) {
+				collapsed.checked = localStorage['collapseResults'];
+			}
+			else {
+				//	Defaults to true
+				collapsed.checked = true;
+			}
+		});
+	}
+
+	//	Edit hosts
+	function addOptionToSelect(optionText, select) {
+		var option = document.createElement('option');
+
+		option.textContent = optionText;
+		select.appendChild(option);
+
+		return option;
+	}
+
+	function removeOptionsFromSelect(select) {
+		var option;
+
+		for (var i = 0; i < select.childNodes.length; i++) {
+			option = select.childNodes[i];
+			if (option.selected === true) {
+				select.removeChild(option);
 			}
 		}
-		if (localStorage['validator'] !== undefined) {
-			validator.value = localStorage['validator'];
-		}
-		if (localStorage['collapseResults'] !== undefined) {
-			collapsed.checked = localStorage['collapseResults'];
-		}
-		else {
-			//	Defaults to true
-			collapsed.checked = true;
-		}
-	}, false);
+	}
 
 	//	Save options
 	function saveOptions() {
-		var validateHostsElm = document.getElementById('validateHosts'),
+		var enableHosts = [],
 			validateHosts = [];
 
-		localStorage['enableHosts'] = enableHosts.value;
-
-		for (var i = 0; i < validateHostsElm.childNodes.length; i++) {
-			validateHosts.push(validateHostsElm.childNodes[i].textContent);
+		for (var i = 0; i < enableHostsElm.childNodes.length; i++) {
+			enableHosts.push(enableHostsElm.childNodes[i].textContent);
 		}
 
+		for (var j = 0; j < validateHostsElm.childNodes.length; j++) {
+			validateHosts.push(validateHostsElm.childNodes[j].textContent);
+		}
+
+		//	Sort hosts alphabetically
+		enableHosts.sort();
+		validateHosts.sort();
+
+		localStorage['enableHosts'] = enableHosts.join(' ');
 		localStorage['validateHosts'] = validateHosts.join(' ');
 		localStorage['validator'] = validator.value;
 		localStorage['collapseResults'] = collapse.checked;
 	}
 
-	//	Attach save and apply events
-	document.getElementById('apply').addEventListener('click', function() {
-		saveOptions();
-	});
+	//	DOM Ready
+	document.addEventListener('DOMContentLoaded', function() {
+		enableHostsElm = $('enableHosts')
+		validateHostsElm = $('validateHosts');
+		//validateHost = $('validateHost');
 
-	document.getElementById('optionsForm').addEventListener('submit', function() {
-		saveOptions();
-		window.close();
-		return false;
-	}, false);
+		//	Load option values into UI
+		loadOptions();
+
+		//	Attach add and remove events for select boxes
+		$('enableAdd').addEventListener('click', function() {
+			var textArea = $('enableHost');
+			addOptionToSelect(textArea.value, enableHostsElm);
+			textArea.value = '';
+		});
+
+		$('enableRemove').addEventListener('click', function() {
+			removeOptionsFromSelect(enableHostsElm);
+		});
+
+		$('validateAdd').addEventListener('click', function() {
+			var textArea = $('validateHost');
+			addOptionToSelect(textArea.value, validateHostsElm);
+			textArea.value = '';
+		});
+
+		$('validateRemove').addEventListener('click', function() {
+			removeOptionsFromSelect(validateHostsElm);
+		});
+
+		//	Attach save and apply events
+		$('apply').addEventListener('click', function() {
+			saveOptions();
+		});
+
+		$('optionsForm').addEventListener('submit', function() {
+			saveOptions();
+			window.close();
+			return false;
+		});
+	});
 })();
