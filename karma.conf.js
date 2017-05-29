260b609868dba791ca46427aa5295fb4db86ee3f
Ian Renyard <ian@renyard.net>
2017-05-29 11:12:47 +0100

    New project structure

--- karma.conf.js
+++ karma.conf.js
@@ -1,55 +1,34 @@
-// Karma configuration
-// Generated on Thu Oct 15 2015 23:49:50 GMT+0100 (BST)
+/* eslint-env node */
 
-module.exports = function(config) {
-  var conf = {
+// Karma configuration
+// Generated on Sat May 27 2017 16:55:20 GMT+0100 (BST)
+
+module.exports = function (config) {
+  config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['qunit'],
-
+    frameworks: ['mocha', 'chai', 'sinon'],
 
     // list of files / patterns to load in the browser
     files: [
-      'test/mocks.js',
-      'test/fixtures/xmlfixtures.js',
-      'test/fixtures/jsonfixtures.js',
       'src/**/*.js',
-      'test/tests.js',
-      {
-          pattern: 'src/**/*',
-          included: false,
-          watched: true
-      },
-      {
-        pattern: 'test/fixtures/**/*',
-        included: false,
-        watched: false
-      }
+      'test/**/*.spec.js'
     ],
-
 
     // list of files to exclude
     exclude: [
     ],
 
-
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'src/**/*.js': ['coverage']
+      'src/**/*.js': ['babel'],
+      'test/**/*.js': ['babel']
     },
-
-
-    proxies: {
-        '/_locales/': '/base/dist/_locales/',
-        '/fixtures/': '/base/test/fixtures/'
-    },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
@@ -57,50 +36,39 @@ module.exports = function(config) {
     reporters: ['progress', 'coverage'],
 
     coverageReporter: {
-		reporters: [
-			{
-				type: 'text'
-			},
-			{
-				type: 'lcovonly'
-			}
-		]
+      reporters: [
+        {
+          type: 'text'
+        },
+        {
+          type: 'lcov'
+        }
+      ]
     },
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
+    autoWatch: false,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [
-		'ChromeCI'
-	],
-
-    customLaunchers: {
-        ChromeCI: {
-            base: 'Chrome',
-            flags: ['--no-sandbox']
-        }
-    },
+    browsers: ['Chrome', 'Opera'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  };
+    singleRun: true,
 
-  config.set(conf);
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
 }
