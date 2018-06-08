beaad8fb466cc3f32952c7d38ab7cfcf5258cb60
Ian Renyard <ian@renyard.net>
2018-06-08 18:16:34 +0100

    Added tests for chromiumStorage

---
+++ test/config/provider/chromiumStorage.spec.js
@@ -0,0 +1,19 @@
+const assert = require('assert')
+
+describe('chromiumStorage', () => {
+  let chromiumStorage
+
+  beforeEach(() => {
+    chromiumStorage = require('../../../src/config/provider/chromiumStorage')
+  })
+
+  it('exports get and set methods', () => {
+    assert.strictEqual(typeof chromiumStorage.get, 'function')
+    assert.strictEqual(typeof chromiumStorage.set, 'function')
+  })
+
+  it('get and set functions do not throw', () => {
+    assert.doesNotThrow(chromiumStorage.get)
+    assert.doesNotThrow(chromiumStorage.set)
+  })
+})
