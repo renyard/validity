9a405e90bc4327755af7995ca0f00e874f7016d3
Ian Renyard <ian@renyard.net>
2018-06-25 21:16:00 +0100

    Implemented error handling for source requests

--- src/source/index.js
+++ src/source/index.js
@@ -1,6 +1,19 @@
 const request = require('superagent')
+const ERRORS = require('../error/constants.json')
 
 module.exports = async (url) => {
-  const response = await request.get(url)
-  return response.text
+  let text
+
+  try {
+    const response = await request.get(url)
+    text = response.text
+  } catch (err) {
+    if (err.response) {
+      text = err.response
+    } else {
+      throw new Error(ERRORS.SOURCE_ERROR)
+    }
+  }
+
+  return text
 }
--- test/unit/source/index.spec.js
+++ test/unit/source/index.spec.js
@@ -29,4 +29,68 @@ describe('source', () => {
 
     assert.strictEqual(result, '<!doctype html>')
   })
+
+  it('returns source for a 404 response', async () => {
+    requestMock = mockSuperagent(request, [{
+      pattern: 'https://host/404.html',
+      fixtures: (match, params, headers, context) => {
+        let error = new Error()
+        error.status = 404
+        error.response = '<!doctype html>'
+
+        throw error
+      }
+    }])
+
+    let result
+
+    try {
+      result = await source('https://host/404.html')
+    } catch (err) {
+      assert.fail('Error thrown')
+    }
+
+    assert.strictEqual(result, '<!doctype html>')
+  })
+
+  it('returns source for an 500 response', async () => {
+    requestMock = mockSuperagent(request, [{
+      pattern: 'https://host/500.html',
+      fixtures: (match, params, headers, context) => {
+        let error = new Error()
+        error.status = 500
+        error.response = '<!doctype html>'
+
+        throw error
+      }
+    }])
+
+    let result
+
+    try {
+      result = await source('https://host/500.html')
+    } catch (err) {
+      assert.fail('Error thrown')
+    }
+
+    assert.strictEqual(result, '<!doctype html>')
+  })
+
+  it('throws error on network error', async () => {
+    requestMock = mockSuperagent(request, [{
+      pattern: 'https://networkError',
+      fixtures: (match, params, headers, context) => {
+        let error = new Error()
+        error.status = undefined
+        throw error
+      }
+    }])
+
+    try {
+      await source('https://networkError')
+      assert.fail('No error thrown')
+    } catch (err) {
+      assert.equal(err.message, 'source_error')
+    }
+  })
 })
